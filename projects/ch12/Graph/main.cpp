bf5ba7c93c24378f57fbad1549f9ad76ba0d16e5
gavkosmig1 <iumanov.vm@phystech.edu>
2023-11-01 09:09:27 +0300

    Update main.cpp

--- projects/ch12/Graph/main.cpp
+++ projects/ch12/Graph/main.cpp
@@ -2,26 +2,26 @@
 #include <sstream>
 #include <stdexcept>
 
-#include <Graph_lib/Graph.h>
-#include <Graph_lib/Simple_window.h>
-#include <Graph_lib/GraphShape.h>
+#include <lib/Graph_lib/Graph.h>
+#include <lib/Graph_lib/GraphShape.h>
+#include <lib/Graph_lib/Simple_window.h>
 
 using namespace Graph_lib;
 
-int main()
+int main ()
 {
 
-    Simple_window win({100, 100}, 800, 800, "LOLZ");
+  Simple_window win({100, 100}, 800, 800, "LOLZ");
 
-    win.wait_for_button();
-    Graph_lib::Arc2 arc{Point{300, 300}, 200, 200, 45.0, 135.0};
-    win.attach(arc);
+  win.wait_for_button();
+  Graph_lib::Arc2 arc{Point{300, 300}, 200, 200, 45.0, 135.0};
+  win.attach(arc);
 
-    Graph_lib::Arc2 arc1{Point{400, 400}, 300, 300, 225, 315};
-    win.attach(arc1);
+  Graph_lib::Arc2 arc1{Point{400, 400}, 300, 300, 225, 315};
+  win.attach(arc1);
 
-    Graph_lib::Mark{Point{200, 200}, 'm'};
+  Graph_lib::Mark{Point{200, 200}, 'm'};
 
-    win.wait_for_button();
-    return 0;
+  win.wait_for_button();
+  return 0;
 }
