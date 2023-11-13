0a5ea18ee4647a4c6dfc3408b3f7b6049dea1634
gavkosmig1 <iumanov.vm@phystech.edu>
2023-11-13 17:16:25 +0300

    uhahaha

--- projects/07/calculator/параша/GodsCalculator/Token.cpp
+++
@@ -1,7 +0,0 @@
-#include "Token.h"
-
-Token::Token() {} //ctor
-
-Token::Token(char kind) : kind(kind), value (0) {} // ctor
-
-Token::Token(char kind, double value) : kind(kind), value(value) {} //ctor
