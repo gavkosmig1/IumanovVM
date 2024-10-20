4e0bdb5a6d475d853b92fd27ff078365d4abd589
gavkosmig1 <iumanov.vm@phystech.edu>
2024-10-20 03:02:10 +0300

    ogo dz2

---
+++ Abongus2.cpp
@@ -0,0 +1,97 @@
+
+#include <cstdlib>
+#include <mpi.h>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <fstream>
+
+void check_for_suc(int ec)
+{
+    if (ec != MPI_SUCCESS)
+    {
+        MPI_Abort(MPI_COMM_WORLD, ec);
+    }
+}
+
+
+double f(double x)
+{
+    double value = std::sqrt(4.0 - x * x);
+    return value;
+}
+
+
+int main(int argc, char **argv)
+{   // Чтение файла с интернета (докапываться запрещено)
+    // std::ifstream inputFile("NateHiggers.dat");
+    // if (!inputFile.is_open()) {
+    //     std::cerr << "Where the file is??" << std::endl;
+    //     return 1;
+    // }
+
+    // int N = 0;
+    // std::string line;
+
+    // while (getline(inputFile, line)) {
+    //     N = std::atoi(line.c_str());
+    // }
+
+    // inputFile.close();
+
+    int st = MPI_Init(&argc, &argv);
+    check_for_suc(st);
+
+    int size, rank;
+
+    int sz = MPI_Comm_size(MPI_COMM_WORLD, &size);
+    check_for_suc(sz);
+    int rk = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    check_for_suc(rk);
+
+    int N;
+    if (rank == 0)
+    {
+        std::ifstream in("N.dat");
+        in >> N;
+        in.close();
+    }
+
+    int bc = MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    check_for_suc(bc);
+
+    double res = 0.0;
+    double cur = 0.0;
+    double ink = 2.0 / N;
+    int part = N / size;
+    cur = rank * part * ink * 1.0;
+
+    for (int i = 0; i < part; i++)
+    {
+        res += (f(cur) + f(cur + ink)) * ink / 2.0;
+        cur += ink;
+    }
+
+    if (rank == size - 1)
+    {
+        if (N % size != 0)
+        {
+            int t = N - part *size;
+            for (int i = 0; i < t-1; i++)
+            {
+                res += (f(cur) + f(cur + ink)) * ink / 2.0;
+                cur += ink;
+            }
+        }
+    }
+
+    double itog;
+    int red = MPI_Reduce(&res, &itog, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    check_for_suc(red);
+    if (rank == 0) {
+        std::cout << "I think the PIE is equal to " << itog << std::endl;
+    }
+
+    MPI_Finalize();
+    return 0;
+}
--- pipipi.cpp
+++ pipipi.cpp
@@ -1,17 +1,70 @@
-#include <math.h>
 #include <iostream>
+#include <cmath>
+#include <mpi.h>
+#include <fstream>
 
-double f(double x) {
-    return sqrt(4-x*x);
-    };
-
-int main() {
-    int N = 100000;
-    double h = 2.0 / N;
-    double S = 0;
-
-    for (int i = 0; i < N-1; i++) {
-        S += (f(i * h) + f(i * h)) * h/2;
+void check_for_suc(int ec)
+{
+    if (ec != MPI_SUCCESS)
+    {
+        MPI_Abort(MPI_COMM_WORLD, rc);
     }
-    std::cout << S;
+}
+
+double func(double x) {
+    return std::sqrt(4 - x * x);
+}
+
+double area(double x, double segment) {
+    return (func(x) + func(x + segment)) * (segment / 2.0);
+}
+
+int main(int argc, char **argv) {
+    int st = MPI_Init(&argc, &argv);
+    check_for_suc(st);
+
+    int rank, size;
+
+    int rk = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    check_for_suc(rk);
+
+    int sz = MPI_Comm_size(MPI_COMM_WORLD, &size);
+    check_for_suc(sz);
+
+    int N;
+    if (rank == 0) {
+        std::ifstream in("N.dat");
+        in >> N;
+        in.close();
+    }
+    int bc = MPI_Bcast(&N, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    check_for_suc(bc);
+    double pi = 0.0;
+    double current_pos = 0.0;
+    double segment = 2.0 / N;
+    int part = N / size;
+    current_pos = rank * part * segment * 1.0;
+    for(int i = 0; i < part; i++){
+        pi += area(current_pos, segment);
+        current_pos += segment;
+    }
+    if (rank == size - 1) {
+        if (N % size != 0) {
+            int t = N - part *size;
+            for (int i = 0; i < t-1; i++) {
+                pi += area(current_pos, segment);
+                current_pos += segment;
+            }
+        }
+    }
+
+    double res;
+    int red = MPI_Reduce(&pi, &res, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    check_for_suc(red);
+    if (rank == 0) {
+        std::cout << "Pi = " << res << std::endl;
+    }
+
+    MPI_Finalize();
+    return 0;
 }
